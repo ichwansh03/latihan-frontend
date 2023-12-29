e2445f8d4082dd38c6123307489d6d0ad73d68e4
Ichwan <ichwansholihin03@gmail.com>
2023-12-29 07:06:04 +0700

    add modules

---
+++ js/modules/message.js
@@ -0,0 +1,7 @@
+const message = () => {
+    const name = "Dewi Fatmawati";
+    const age = 22;
+    return name + ' is '+age+' years old.';
+};
+
+export default message;
---
+++ js/modules/user.js
@@ -0,0 +1,4 @@
+const name = "Ichwan Sholihin"
+const age = 22;
+
+export {name, age};
